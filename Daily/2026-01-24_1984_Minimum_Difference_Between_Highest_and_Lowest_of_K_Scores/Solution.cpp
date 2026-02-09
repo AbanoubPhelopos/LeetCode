fd2e7c12235c4b0893615e15a30595cd5846e03c
abanoub saweris <abanoubphelopos12@gmail.com>
2026-02-09 23:54:59 +0200

    daily leetcode 24 jan 2026

---
+++ Daily/2026-01-24_1984_Minimum_Difference_Between_Highest_and_Lowest_of_K_Scores/Solution.cpp
@@ -0,0 +1,17 @@
+
+using namespace std;
+
+class Solution {
+public:
+  static int minimumDifference(vector<int> &nums, int k) {
+    const int n = nums.size();
+    if (k == 1)
+      return 0;
+    sort(nums.begin(), nums.end());
+    int diff = INT_MAX;
+    for (int l = 0, r = k - 1; r < n; r++, l++) {
+      diff = min(diff, nums[r] - nums[l]);
+    }
+    return diff;
+  }
+};
